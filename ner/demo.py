914f29a2e6459b22e039ee701a6cf08b5433c73b
sandeepreddygantla <reddysandeep0904@gmail.com>
2024-04-24 14:50:37 -0500

    Data Preprocessing completed

--- ner/demo.py
+++ ner/demo.py
@@ -1,6 +1,7 @@
 from ner.components.data_ingestion import DataIngestion
 from ner.config.configuration import Configuration
 from ner.components.data_validation import DataValidation
+from ner.components.data_prepration import DataPreprocessing
 
 project_config = Configuration()
 ingestion = DataIngestion(project_config.get_data_ingestion_config())
@@ -18,3 +19,17 @@ def validate_check():
         else:
             return True
 
+def validate_check():
+    for i in check[0]:
+        if i == False:
+            return False
+        else:
+            return True
+
+
+if validate_check() == True:
+    processed_data = DataPreprocessing(
+        data_preprocessing_config=project_config.get_data_preprocessing_config(),
+        data=en_data,
+    )
+    
